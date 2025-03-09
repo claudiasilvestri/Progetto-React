fcdecdea3b0322600cd1d3bb2c37a25818020748
Claudia <cl.silvestri@yahoo.com>
2025-03-09 17:19:24 +0100

    versione corretta

---
+++ src/Layout/index.jsx
@@ -0,0 +1,19 @@
+import { Link, Outlet } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/games/genre">Genre</Link></li> 
+        <li><Link to="/games/game-name">Game</Link></li>
+      </ul>
+      <Outlet /> 
+    </nav>
+  );
+};
+export default Navbar;
+
+
+
+
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Genre from "../pages/Genre";
-import Game from "../Pages/Game";
-import Navbar from "../Layout/Navbar";
+import Game from "../pages/Game";
+import Navbar from "../Layout";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<Navbar />}>
-      <Route path="/" element={<Home />} />
+    <Route path="/" element={<Navbar />}>
+      <Route index element={<Home />} />
       <Route path="games/genre" element={<Genre />} />
       <Route path="games/game-name" element={<Game />} />
     </Route>
